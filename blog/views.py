d20190f96e1f380f45f61a9e565d9be06172ee8f
g1721 <g.avila-2002@hotmail.com>
2023-05-20 18:08:11 -0400

    editar los post

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 # Create your views here idiot.
+
 
 class BlogListView(ListView):
     model = Post
     template_name = "home.html"
 
+
 class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["tittle", "body", "author"]
+
+
+class BlogEditView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["tittle", "body"]
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
