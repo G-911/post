de9a349336ba183fcf396c2dcfe49334163b54fb
g1721 <g.avila-2002@hotmail.com>
2023-05-27 17:00:12 -0400

    login, logout

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
